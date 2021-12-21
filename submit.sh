ceb9af4ffb6a7d7e31065c809b0e7a80b67763e6
alpv95 <59vpla@gmail.com>
2021-12-21 07:05:22 -0800

    file_cleanup

--- submit.sh
+++ submit.sh
@@ -2,24 +2,14 @@
 #
 #SBATCH --job-name=S5
 #SBATCH --output=slurm/%A.out
-###SBATCH --error=current_%A_%a.err
+####SBATCH --error=current_%A_%a.err
 #SBATCH --time=800:00
 ##SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=15
+#SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=5G
 #SBATCH --partition=owners
 
-ml gsl
-ml python/3.6.1
-ml py-scipy/1.1.0_py36
-ml viz
-ml py-matplotlib/2.1.2_py36
-ml cmake/3.13.1
-ml py-numpy/1.17.2_py36
-ml gcc/8.1.0
-
-pwd
-python3 -u sscpol/fit.py --method ps --blazar S5flare
+python3 -u fit.py --method standard
 
 
